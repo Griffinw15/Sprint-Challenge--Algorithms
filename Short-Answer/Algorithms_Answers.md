d9fc8ca7614b7cdc64563d2091c58600cc4f3715
Griffinw15 <wilson.griffin15@gmail.com>
2020-08-21 10:49:15 -0700

    okay

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -4,12 +4,22 @@
 
 a)
 
+O(log n)
+
 
 b)
+
+O(n log n)
 
 
 c)
 
+O(n)
+
 ## Exercise II
 
+should try a binary search
 
+If the building is 100 floors, we can guess floor 50 first. The egg will either break or not. If it breaks I then would guess floor 25, if it does not I would go to 75. So on and so forth, worse case scenario I go to 50 floors. This cuts workload in half.
+
+I think binary search has a O(log n) runtime
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,25 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+counter = []
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+
+    #counter = []
+
+    if len(word) <= 1:
+        return len(counter)
+
+    #for i in range(len(word) - 1):
+    if word[0] == "t" and word[1] == "h":
+        #counter += 1
+        counter.append('a')
+        return count_th(word[2:])
+        
+    else:
+        return count_th(word[1:])
+
+word = "abcthxyz"
+
+print(count_th(word))
